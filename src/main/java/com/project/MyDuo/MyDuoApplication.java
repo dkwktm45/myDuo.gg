7b5692331d0799521aac2f1dd2e1c5144d3c7cc6
ensk26 <tjensk26@gmail.com>
2022-08-05 22:42:19 +0900

    initial

---
+++ src/main/java/com/project/MyDuo/MyDuoApplication.java
@@ -0,0 +1,13 @@
+package com.project.MyDuo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDuoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDuoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/MyDuo/MyDuoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.MyDuo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDuoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
