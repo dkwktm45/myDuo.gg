d48b2d14c3dc9db19e0a5eacb32a09e8051b0b33
dkwktm45 <dkwktm45@naver.com>
2022-08-23 15:34:31 +0900

    jpa 주석처리

--- src/main/java/com/project/MyDuo/entity/Lane.java
+++ src/main/java/com/project/MyDuo/entity/Lane.java
@@ -3,18 +3,15 @@ package com.project.MyDuo.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+//import javax.persistence.*;
 
-@Entity @Getter @Setter
+//@Entity
+@Getter @Setter
 public class Lane {
-    @Id @GeneratedValue
-    @Column(name = "LANE_ID")
+//    @Id @GeneratedValue
+//    @Column(name = "LANE_ID")
     private Long id;
-
-
-    
-    @Enumerated(EnumType.STRING)
+//    @Enumerated(EnumType.STRING)
     private LaneType laneType;
-
     private Integer count;
 }
