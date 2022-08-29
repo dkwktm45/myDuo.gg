22840ccd5dd9e12546cbbdfe9379926f05a04391
Jinyoung <102174146+jinyoung4478@users.noreply.github.com>
2022-08-29 19:46:01 +0900

    로그아웃 버튼 이동
    메인 페이지에 있던 로그아웃 버튼을 프로필 페이지로 이동

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,9 +1,43 @@
 import NavBar from "components/NavBar";
+import styled from "styled-components";
+import { useResetRecoilState } from "recoil";
+import { LoginState } from "atoms";
+import { useNavigate } from "react-router-dom";
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: calc(100vh - 120px);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 120px;
+`;
+
+const TestButton = styled.button`
+  border: none;
+  width: 200px;
+  height: 50px;
+  color: white;
+  background-color: ${(props) => props.theme.lolTextColor};
+  margin: 30px 0;
+  cursor: pointer;
+`;
 
 function Profile() {
+  const navigate = useNavigate();
+  const onClickLogOut = () => {
+    logOut();
+    navigate("/login");
+  };
+
+  const logOut = useResetRecoilState(LoginState);
+
   return (
     <>
       <NavBar />
+      <Wrapper>
+        <TestButton onClick={onClickLogOut}>로그아웃</TestButton>
+      </Wrapper>
     </>
   );
 }
