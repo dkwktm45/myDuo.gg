2d225f116b26f07907ff3815925d1302af63629b
ensk26 <tjensk26@gmail.com>
2022-09-02 22:25:14 +0900

    swagger 구성 및 SecurityConfig 수정

--- backend/src/main/java/com/project/MyDuo/config/SwaggerConfig.java
+++ backend/src/main/java/com/project/MyDuo/config/SwaggerConfig.java
@@ -2,13 +2,22 @@ package com.project.MyDuo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import java.util.Arrays;
+import java.util.List;
+
+@EnableWebMvc
 @Configuration
 public class SwaggerConfig {
 
@@ -16,8 +25,10 @@ public class SwaggerConfig {
 	public Docket api() {
 		return new Docket(DocumentationType.OAS_30)
 				.useDefaultResponseMessages(false)
+				.securityContexts(Arrays.asList(securityContext()))
+				.securitySchemes(Arrays.asList(apiKey()))
 				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.project.MyDuo.controller"))
+				.apis(RequestHandlerSelectors.basePackage("com.project.MyDuo"))
 				.paths(PathSelectors.any())
 				.build()
 				.apiInfo(apiInfo());
@@ -25,10 +36,25 @@ public class SwaggerConfig {
 
 	private ApiInfo apiInfo() {
 		return new ApiInfoBuilder()
-				.title("Practice Swagger")
-				.description("practice swagger config")
+				.title("API Documentation")
+				.description("Swagger API Documentation")
 				.version("1.0")
 				.build();
+	}
+
+	private ApiKey apiKey() {
+		return new ApiKey("Authorization", "Authorization", "header");
+	}
+
+	private SecurityContext securityContext() {
+		return SecurityContext.builder().securityReferences(defaultAuth()).build();
+	}
+
+	private List<SecurityReference> defaultAuth() {
+		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+		authorizationScopes[0] = authorizationScope;
+		return Arrays.asList(new SecurityReference("Authorization", authorizationScopes));
 	}
 
 }
