153037f2577f9bdaa68367a2ae25318775b3d3fc
Jinyoung <102174146+jinyoung4478@users.noreply.github.com>
2022-09-02 17:42:28 +0900

    프로필 페이지 업데이트

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -5,7 +5,7 @@ import { LoginState } from "atoms";
 import { useNavigate } from "react-router-dom";
 import Alarm from "components/Alarm";
 
-const Wrapper = styled.div`
+const Container = styled.div`
   width: 100vw;
   height: calc(100vh - 120px);
   display: flex;
@@ -24,6 +24,20 @@ const TestButton = styled.button`
   cursor: pointer;
 `;
 
+const Wrapper = styled.div`
+  width: 1030px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: ${(props) => props.theme.lolBgColorLight};
+`;
+
+const Header = styled.div`
+  width: 80%;
+  height: 50px;
+  margin: 10px 0;
+`;
+
 function Profile() {
   const navigate = useNavigate();
   const onClickLogOut = () => {
@@ -36,11 +50,19 @@ function Profile() {
   return (
     <>
       <NavBar />
-      <Wrapper>
-        <TestButton onClick={onClickLogOut}>로그아웃</TestButton>
-      </Wrapper>
+      <Container>
+        <Wrapper>
+          <Header>
+            <span>Profile Picture</span>
+            <span>계정 닉네임</span>
+            <span>자기 소개</span>
+          </Header>
+          <TestButton onClick={onClickLogOut}>로그아웃</TestButton>
+        </Wrapper>
+      </Container>
       <Alarm />
     </>
   );
 }
+
 export default Profile;
