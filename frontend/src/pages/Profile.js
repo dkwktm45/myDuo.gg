d6680f77d7fab3ef1d3455c71ee89aa707e92b23
minseon33 <109520441+minseon33@users.noreply.github.com>
2022-09-05 11:58:02 +0900

    프로필 헤더 수정중

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 import Alarm from "components/Alarm";
 
 const Container = styled.div`
-  width: 50vw;
+  width: 100vw;
   height: calc(100vh - 120px);
   display: flex;
   flex-direction: column;
@@ -29,14 +29,70 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: ${(props) => props.theme.lolBgColorLight};
 `;
 
 const Header = styled.div`
+  border:1px solid aqua;
   width: 80%;
-  height: 50px;
+  height: 150px;
   margin: 10px 0;
+  display:flex;
+  flex-direction:row;
+  justify-content:space-beetween;
 `;
+
+const SumitButton = styled.button`
+border: none;
+width: 50px;
+height: 20px;
+color: white;
+background-color: ${(props) => props.theme.lolTextColor};
+margin: 30px 0;
+cursor: pointer;
+border-radius:5px
+`;
+
+const ProfilePicture = styled.div`
+border:2px solid tomato;
+width :150px;
+height : 150px;
+
+`;
+
+const UserName = styled.div`
+border:2px solid green;
+width :150px;
+height : 150px;
+background-color: ${(props) => props.theme.lolTextColor};
+`;
+
+const IntroduceYourself = styled.form`
+border : 2px solid green;
+width :150px;
+height : 150px;
+
+`;
+
+function Introduce() {
+  const [introduce,setIntro] = useState("");
+  const handleIntro = (e) => {
+    setIntro(e.target.value);
+  };
+  const handleSudmlt = (e) => {
+
+  };
+  return(
+    <form coSubmit = { handleSudmlt}>
+      <label>
+        자기소개
+      </label>
+      <input 
+        type="text" 
+        value={introduce} 
+        onChange={handleIntro}/>
+    </form>
+  )
+}
 
 function Profile() {
   const navigate = useNavigate();
@@ -49,13 +105,15 @@ function Profile() {
 
   return (
     <>
-      <NavBar />
+      
       <Container>
+      <NavBar />
         <Wrapper>
           <Header>
-            <span>Profile Picture</span>
-            <span>계정 닉네임</span>
-            <span>자기 소개</span>
+            <ProfilePicture>Profile Picture</ProfilePicture>
+            <UserName>계정 닉네임</UserName>
+            <IntroduceYourself>자기 소개</IntroduceYourself>
+            <SumitButton>수정</SumitButton>
           </Header>
           <TestButton onClick={onClickLogOut}>로그아웃</TestButton>
         </Wrapper>
