f38492f650ad5b15713e3d8b6f6ed3b2ccacc562
dkwktm45 <dkwktm45@naver.com>
2022-09-10 19:07:46 +0900

    #9 socket security 추가

---
+++ backend/src/main/java/com/project/MyDuo/config/SockSecurityConfig.java
@@ -0,0 +1,15 @@
+package com.project.MyDuo.config;
+
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+
+@Configuration
+public class SockSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+
+	@Override
+	protected boolean sameOriginDisabled() {
+		return true;
+	}
+
+}
